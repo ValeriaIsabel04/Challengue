a04a5f4e60f302db1a47971821f67a9b82ce32cc
Valeria Isabel <vvasquezbarragan@gmail.com>
2025-03-11 21:59:47 -0500

    Actualiza el app.js

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-let sorteoRealizado= false;
-function verificarIntento() {
-    let input= document.getElementById("amigo");
-    if (sorteoRealizado) {
-        alert("El sorteo ya ha sido realizado. Por favor, reinicia la lista para agregar nuevos participantes.");
-        input.value = ""; // Limpiar el campo de entrada
-        return;
-}
 
